2187f89da9da192afa8f98def88bfd88cf2aab77
samuelmorenov <xamelsamuel@gmail.com>
2021-03-22 12:03:41 +0100

    SDI-2021-1005-7.4-Recursos estáticos, peticiones POST y parámetros, Enrutamiento y comodines.

--- app.js
+++ app.js
@@ -2,6 +2,12 @@
 var express = require('express');
 var app = express();
 
+let bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(express.static('public'));
+
 // Variables
 app.set('port', 8081);
 
@@ -10,6 +16,6 @@ require("./routes/rusuarios.js")(app); // (app, param1, param2, etc.)
 require("./routes/rcanciones.js")(app); // (app, param1, param2, etc.)
 
 // lanzar el servidor
-app.listen(app.get('port'), function() {
+app.listen(app.get('port'), function () {
     console.log("Servidor activo en http://localhost:8081/");
 });
--- routes/rcanciones.js
+++ routes/rcanciones.js
@@ -6,6 +6,16 @@ module.exports = function (app) {
         res.send(respuesta);
     });
 
+    app.post("/cancion", function (req, res) {
+        res.send("Canción agregada:" + req.body.nombre + "<br>"
+            + " genero :" + req.body.genero + "<br>"
+            + " precio: " + req.body.precio);
+    });
+
+    app.get('/promo*', function (req, res) {
+        res.send('Respuesta patrón promo* ');
+    });
+
     app.get('/suma', function (req, res) {
         let respuesta = parseInt(req.query.num1) + parseInt(req.query.num2);
         res.send(String(respuesta));
