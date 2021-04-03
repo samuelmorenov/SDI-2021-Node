e1200350f6a29be02a77a94ba0dc3f2dad8346dd
samuelmorenov <xamelsamuel@gmail.com>
2021-04-03 21:37:15 +0200

    SDI-2021-1005-8.2-Arquitectura para el acceso a datos.

--- app.js
+++ app.js
@@ -9,6 +9,9 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static('public'));
 
+let gestorBD = require("./modules/gestorBD.js");
+gestorBD.init(app,mongo);
+
 // Variables
 app.set('port', 8081);
 let fs = require('fs');
@@ -16,9 +19,9 @@ let pass = fs.readFileSync('pass.txt', 'utf-8');
 app.set('db','mongodb://admin:'+pass+'@tiendamusica-shard-00-00-8d9nh.mongodb.net:27017,tiendamusica-shard-00-01-8d9nh.mongodb.net:27017,tiendamusica-shard-00-02-8d9nh.mongodb.net:27017/test?ssl=true&replicaSet=tiendamusica-shard-0&authSource=admin&retryWrites=true&w=majority');
 
 //Rutas/controladores por lógica
-require("./routes/rusuarios.js")(app, swig, mongo);
-require("./routes/rcanciones.js")(app, swig, mongo);
-require("./routes/rautores.js")(app, swig, mongo);
+require("./routes/rusuarios.js")(app, swig, gestorBD);
+require("./routes/rcanciones.js")(app, swig, gestorBD);
+require("./routes/rautores.js")(app, swig, gestorBD);
 
 // lanzar el servidor
 app.listen(app.get('port'), function () {
