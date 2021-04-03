9d494f336e0d9a66f38c2fdd3c0514b5718c784f
samuelmorenov <xamelsamuel@gmail.com>
2021-04-03 22:40:38 +0200

    SDI-2021-1005-8.4-Registro de usuario, autenticación y uso de sesión.

--- modules/gestorBD.js
+++ modules/gestorBD.js
@@ -1,17 +1,16 @@
 module.exports = {
-    mongo: null, app: null,
+    mongo: null,
+    app: null,
     init: function (app, mongo) {
         this.mongo = mongo;
         this.app = app;
     },
-
-
     obtenerCanciones: function (criterio, funcionCallback) {
         this.mongo.MongoClient.connect(this.app.get('db'), function (err, db) {
             if (err) {
                 funcionCallback(null);
             } else {
-                let collection = db.collection('canciones');
+                var collection = db.collection('canciones');
                 collection.find(criterio).toArray(function (err, canciones) {
                     if (err) {
                         funcionCallback(null);
@@ -23,19 +22,55 @@ module.exports = {
             }
         });
     },
-
-
     insertarCancion: function (cancion, funcionCallback) {
         this.mongo.MongoClient.connect(this.app.get('db'), function (err, db) {
             if (err) {
                 funcionCallback(null);
             } else {
-                let collection = db.collection('canciones');
+                var collection = db.collection('canciones');
                 collection.insert(cancion, function (err, result) {
                     if (err) {
                         funcionCallback(null);
                     } else {
                         funcionCallback(result.ops[0]._id);
+                    }
+                    db.close();
+                });
+            }
+        });
+    },
+
+
+    insertarUsuario: function (usuario, funcionCallback) {
+        this.mongo.MongoClient.connect(this.app.get('db'), function (err, db) {
+            if (err) {
+                funcionCallback(null);
+            } else {
+                let collection = db.collection('usuarios');
+                collection.insert(usuario, function (err, result) {
+                    if (err) {
+                        funcionCallback(null);
+                    } else {
+                        funcionCallback(result.ops[0]._id);
+                    }
+                    db.close();
+                });
+            }
+        });
+    },
+
+
+    obtenerUsuarios: function (criterio, funcionCallback) {
+        this.mongo.MongoClient.connect(this.app.get('db'), function (err, db) {
+            if (err) {
+                funcionCallback(null);
+            } else {
+                let collection = db.collection('usuarios');
+                collection.find(criterio).toArray(function (err, usuarios) {
+                    if (err) {
+                        funcionCallback(null);
+                    } else {
+                        funcionCallback(usuarios);
                     }
                     db.close();
                 });
