a22cd11e6778e47ff3ccee7daa2549ffc911109c
samuelmorenov <xamelsamuel@gmail.com>
2021-04-15 22:58:30 +0200

    SDI-2021-1005-10.5-SW Consumir SW desde NodeJS.

--- routes/rcanciones.js
+++ routes/rcanciones.js
@@ -92,18 +92,33 @@ module.exports = function (app, swig, gestorBD) {
                                 req.session.error = "Error al obtener la cancion";
                                 res.redirect('/error');
                             } else {
-                                let sepuedecomprar = true;
-                                if (compras.length != 0 || canciones[0].autor == req.session.usuario) {
-                                    sepuedecomprar = false;
+
+                                let configuracion = {
+                                    url: "https://www.freeforexapi.com/api/live?pairs=EURUSD",
+                                    method: "get",
+                                    headers: {
+                                        "token": "ejemplo",
+                                    }
                                 }
-                                let respuesta = swig.renderFile('views/bcancion.html',
-                                    {
+                                let rest = app.get("rest");
+                                rest(configuracion, function(error, response, body) {
+                                    console.log("cod: " + response.statusCode + " Cuerpo :" + body);
+                                    let objetoRespuesta = JSON.parse(body);
+                                    let cambioUSD = objetoRespuesta.rates.EURUSD.rate;
+                                    // nuevo campo "usd"
+                                    canciones[0].usd = cambioUSD * canciones[0].precio;
+
+                                    let sepuedecomprar = true;
+                                    if (compras.length != 0 || canciones[0].autor == req.session.usuario) {
+                                        sepuedecomprar = false;
+                                    }
+                                    let respuesta = swig.renderFile('views/bcancion.html', {
                                         puedecomprar: sepuedecomprar,
                                         comentario: comentarios,
                                         cancion: canciones[0]
                                     });
-                                res.send(respuesta);
-
+                                    res.send(respuesta);
+                                });
                             }
                         });
                     }
