7bdc1b19783853fd94a63725c059f11c6232a14e
samuelmorenov <xamelsamuel@gmail.com>
2021-04-19 12:51:32 +0200

    SDI-2021-1005-sesión10-Ejercicio-complementario1

--- routes/rapicanciones.js
+++ routes/rapicanciones.js
@@ -1,4 +1,6 @@
 module.exports = function (app, gestorBD) {
+
+    //Listar canciones
     app.get("/api/cancion", function (req, res) {
         gestorBD.obtenerCanciones({}, function (canciones) {
             if (canciones == null) {
@@ -11,6 +13,7 @@ module.exports = function (app, gestorBD) {
         });
     });
 
+    //Obtener una cancion
     app.get("/api/cancion/:id", function (req, res) {
         var criterio = {"_id": gestorBD.mongo.ObjectID(req.params.id)}
         gestorBD.obtenerCanciones(criterio, function (canciones) {
@@ -24,8 +27,11 @@ module.exports = function (app, gestorBD) {
         });
     });
 
+    //Borrar una cancion
     app.delete("/api/cancion/:id", function (req, res) {
         var criterio = {"_id": gestorBD.mongo.ObjectID(req.params.id)}
+
+        comprobarAutoria(req.params.id, res, function () {
         gestorBD.eliminarCancion(criterio, function (canciones) {
             if (canciones == null) {
                 res.status(500);
@@ -35,15 +41,24 @@ module.exports = function (app, gestorBD) {
                 res.send(JSON.stringify(canciones));
             }
         });
+        });
     });
 
+    //Añadir una cancion
     app.post("/api/cancion", function (req, res) {
+        let usuario = res.usuario;
         var cancion = {
             nombre: req.body.nombre,
             genero: req.body.genero, precio:
             req.body.precio,
+            autor: usuario
         }
         // ¿Validar nombre, genero, precio?
+
+        if(!comprobarDatosCancion(cancion, res)){
+            return
+        }
+
         gestorBD.insertarCancion(cancion, function (id) {
             if (id == null) {
                 res.status(500);
@@ -55,7 +70,10 @@ module.exports = function (app, gestorBD) {
         });
     });
 
+    //Modificar una cancion
     app.put("/api/cancion/:id", function(req, res) {
+
+        comprobarAutoria(req.params.id, res, function () {
 
         let criterio = { "_id" : gestorBD.mongo.ObjectID(req.params.id) };
 
@@ -66,6 +84,11 @@ module.exports = function (app, gestorBD) {
             cancion.genero = req.body.genero;
         if ( req.body.precio != null)
             cancion.precio = req.body.precio;
+
+        if(!comprobarDatosCancion(cancion, res)){
+            return
+        }
+
         gestorBD.modificarCancion(criterio, cancion, function(result) {
             if (result == null) {
                 res.status(500);
@@ -80,8 +103,10 @@ module.exports = function (app, gestorBD) {
                 })
             }
         });
+        });
     });
 
+    //Autentificarse
     app.post("/api/autenticar/", function (req, res) {
         let seguro = app.get("crypto").createHmac('sha256', app.get('clave'))
             .update(req.body.password).digest('hex');
@@ -112,5 +137,47 @@ module.exports = function (app, gestorBD) {
             }
         })
     });
+
+    //Funcion auxiliar de comprobacion de validez de datos de una cancion
+    function comprobarDatosCancion(cancion, res) {
+
+        if (String(cancion.nombre).length <= 2 || String(cancion.genero).length <= 2) {
+            res.status(400);
+            res.json({
+                error: "Los campos de nombre y genero deben tener al menos 3 caracteres"
+            });
+            return false;
+        }
+        if (cancion.precio <= 0) {
+            res.status(400);
+            res.json({
+                error: "El precio debe tener un valor positivo"
+            });
+            return false;
+        }
+        return true;
+    };
+
+    //Funcion auxiliar de comprobacion de existencia y autoria de una cancion
+    function comprobarAutoria(idCancion, res, callback){
+        var criterio = {"_id": gestorBD.mongo.ObjectID(idCancion)}
+        let usuario = res.usuario;
+
+        gestorBD.obtenerCanciones(criterio, function (canciones) {
+            if (canciones == null) {
+                res.status(404);
+                res.json({
+                    error: "La cancion no existe"
+                });
+            } else if (canciones[0].autor !== usuario) {
+                res.status(403);
+                res.json({
+                    error: "Debes ser el autor de la cancion"
+                });
+            } else {
+                callback();
+            }
+        })
+    }
 };
 
